4b2db23821318afaebf51b43e78066e02e81616f
shanmugamr <shanmugamr@host.docker.internal>
2019-09-10 17:02:16 -0500

    View MAR functionality added

---
+++ WebContent/marResults.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1" name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"
+	type="text/css" />
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<title>UTA Mac FMS</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ src/com/group4/macfms/data/MarDAO.java
@@ -0,0 +1,42 @@
+package com.group4.macfms.data;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.group4.macfms.model.MarDetails;
+import com.group4.macfms.util.SQLConnection;
+
+public class MarDAO {
+	static SQLConnection DBMgr = SQLConnection.getInstance();
+	Statement stmt = null;
+	Connection conn = SQLConnection.getDBConnection();
+
+	public MarDetails retrieveMarDetails(String date) {
+		MarDetails mar = new MarDetails();
+		String queryString = "SELECT * from mac_fms.mar where assigned_date = '"+date+"';";
+		System.out.println("Printing mar query..."+queryString);
+		try {
+			stmt = conn.createStatement();
+			ResultSet marList = stmt.executeQuery(queryString);
+			while (marList.next()) {
+				mar.setMarNumber(marList.getString("mar_number"));
+				mar.setFacilityType(marList.getString("facility_type"));
+				mar.setReservationId(marList.getString("reservation_id_fk"));
+				mar.setReportedBy(marList.getString("reported_by_fk"));
+				mar.setUrgency(marList.getString("urgency"));
+				mar.setDescription(marList.getString("description"));
+				mar.setDateCreated(marList.getString("date_created"));
+				mar.setAssignedTo(marList.getString("assigned_to"));
+				mar.setAssignedDate(marList.getString("assigned_date"));
+				mar.setEstimatedTime(marList.getString("estimated_time"));
+				mar.setMarStatus(marList.getString("mar_status"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}				
+		return mar;
+	}
+
+}
